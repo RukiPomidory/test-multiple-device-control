d0ce6078d389ea7ec9f36e52acc259a4a682fa46
Arseniy <RukiPomidory@yandex.ru>
2022-08-13 17:19:10 +0300

    Реализовал плавное изменение

--- Assets/Scripts/Command/SmoothCommand.cs
+++ Assets/Scripts/Command/SmoothCommand.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
+using Utils;
 
 namespace DeviceControl
 {
@@ -12,16 +13,44 @@ namespace DeviceControl
 
         public float Duration { get; protected set; }
 
-        public SmoothCommand(Device device)
+        public SmoothCommand(Device device, float duration = 1.5f)
         {
             this.device = device;
+            Duration = duration;
         }
         
         public override void Execute(Vector3 target)
         {
-            throw new NotImplementedException();
-        }
+            var startTime = DeviceUtils.LocalTime();
+            var startValue = device.Position;
+            var delta = DeviceUtils.GetTaskDeltaMilliseconds();
+            
+            // Чтобы точно успеть все провернуть
+            var duration = Duration * 0.98f;
+            
+            Task.Run(() =>
+            {
+                float progress = 0;
 
+                while (progress < 1)
+                {
+                    var time = DeviceUtils.LocalTime();
+                    progress = (time - startTime) / duration;
+                    progress = Mathf.Clamp01(progress);
+                
+                    var newValue = startValue * (1 - progress) + target * progress;
+
+                    if (progress >= 1)
+                        newValue = target;
+                    
+                    device.SetPosition(newValue);
+                    
+                    
+                    Thread.Sleep(delta);
+                }
+            });
+        }
+        
         public override void Stop()
         {
             throw new NotImplementedException();
---
+++ Assets/Scripts/Utils/DeviceUtils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Utils
+{
+    public static class DeviceUtils
+    {
+        private static double startTime;
+
+        static DeviceUtils()
+        {
+            startTime = Time();
+        }
+
+        public static double Time()
+        {
+            var totalMilliseconds = TimeSpan.FromTicks(DateTime.Now.Ticks).TotalMilliseconds;
+            var seconds = totalMilliseconds / 1000;
+
+            return seconds;
+        }
+        
+        public static float LocalTime()
+        {
+            var localTime = Time() - startTime;
+
+            return (float) localTime;
+        }
+
+        public static int GetTaskDeltaMilliseconds()
+        {
+            // 10 мс для 100 фпс внутри тасков
+            return 10;
+        }
+    }
+}
